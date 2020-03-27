7566debe6fb423b6099bf01345f67fd154463c45
sy-records <52o@qq52o.cn>
2020-03-27 17:32:20 +0800

    remove

--- src/Server/WebSocket.php
+++ src/Server/WebSocket.php
@@ -35,11 +35,6 @@ class WebSocket
 
         $this->_server->on('start', [$this, 'onStart']);
         $this->_server->on('workerStart', [$this, 'onWorkerStart']);
-//        $this->_server->on('open', [$this, 'onOpen']);
-//        $this->_server->on('message', [$this, 'onmessage']);
-//        $this->_server->on('handShake', [$this, 'onHandShake']);
-//        $this->_server->on('request', [$this, 'onRequest']);
-//        $this->_server->on('close', [$this, 'onClose']);
         foreach ($wsConfig['callbacks'] as $eventKey => $callbackItem) {
             [$class, $func] = $callbackItem;
             $this->_server->on($eventKey, [$class, $func]);
@@ -58,28 +53,4 @@ class WebSocket
         $this->_route = Route::getInstance();
         Listener::getInstance()->listen('workerStart', $server, $workerId);
     }
-
-//    public function onOpen(Server $server, $request)
-//    {
-//        Listener::getInstance()->listen('open', $server, $request);
-////        echo "server: handshake success with fd{$request->fd}\n";
-//    }
-//
-//    public function onMessage(Server $server, $frame)
-//    {
-//        Listener::getInstance()->listen('message', $server, $frame);
-////        echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
-////        $server->push($frame->fd, 'this is server');
-//    }
-//
-//    public function onRequest(\Swoole\Http\Request $request, \Swoole\Http\Response $response)
-//    {
-//        Listener::getInstance()->listen('message', $request, $response);
-////        $this->_route->dispatch($request, $response);
-//    }
-//
-//    public function onClose(Server $server, $fd)
-//    {
-//        Listener::getInstance()->listen('close', $server, $fd);
-//    }
 }
